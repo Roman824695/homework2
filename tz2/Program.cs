f3cef9a695198bbdea062e2062bff365dc2e907b
Tsykalo Roman <u15921@mail.ru>
2022-10-19 14:08:22 +0400

    Закончил практическое задание второго семинара

---
+++ tz2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите трёхзначное число :->" );
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num > 99 && num < 1000)
+{
+    int number = num % 10;
+    int N = number % 10;
+    Console.WriteLine("Третья цифра" + "->" + N);
+}
+else
+{
+Console.WriteLine("Не верное число");
+}
+
---
+++ tz3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите день недели :->" );
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num > 0 && num < 6)
+{
+Console.WriteLine("НЕТ");
+}
+else if(num > 5 && num < 8)
+{
+Console.WriteLine("ДА");
+} 
+else
+{
+Console.WriteLine("Нет такого дня недели");
+}
+
+
+    
